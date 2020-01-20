811ec8ceb873c46d6ec510a7612bbe07b63866c7
Oleg Goloborodko <goloborodko@inbox.ru>
2020-01-20 20:32:24 +0300

    refactoring spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,14 +4,12 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { create_list(:question, 3) }
 
-    it 'populates an array of all questions' do
-      get :index
+    before { get :index }
 
+    it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
     end
     it 'renders index views' do
-      get :index
-
       expect(response).to render_template :index
     end
   end
