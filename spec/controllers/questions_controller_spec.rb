1b0bfc6ec141790d9da8ac6501c6d3ce8d2c819d
Oleg Goloborodko <goloborodko@inbox.ru>
2020-01-20 20:29:38 +0300

    refactoring spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
-    it 'populates an array of all questions' do
-      questions = create_list(:question, 3)
+    let(:questions) { create_list(:question, 3) }
 
+    it 'populates an array of all questions' do
       get :index
 
       expect(assigns(:questions)).to match_array(questions)
