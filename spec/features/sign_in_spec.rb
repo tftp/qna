0f6ac38375b0d6b1a07a3ce433998c85df6169da
Oleg Goloborodko <goloborodko@inbox.ru>
2020-01-23 21:26:59 +0300

    refactoring acceptance test for User

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -5,18 +5,19 @@ feature 'User can sign in', %q{
   As an unauthenticated user
   I'd like to be able to sign in
 } do
-  scenario 'Registered user tries to sign in' do
-    User.create!(email: 'user@test.com', password: '123456')
 
-    visit new_user_session_path
-    fill_in 'Email', with: 'user@test.com'
-    fill_in 'Password', with: '123456'
+  given(:user) { User.create!(email: 'user@test.com', password: '123456') }
+
+  background { visit new_user_session_path }
+
+  scenario 'Registered user tries to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
     click_on 'Log in'
 
     expect(page).to have_content 'Signed in successfully.'
   end
   scenario 'Unregistered user tries to sign in' do
-    visit new_user_session_path
     fill_in 'Email', with: 'wrong@test.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
