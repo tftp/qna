390d1c7f4d3dba369f6ce9375bb08e4a5ec25ed8
Oleg Goloborodko <goloborodko@inbox.ru>
2020-01-26 17:26:54 +0300

    acceptance spec for users log_out

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  As an unauthenticated user
+  I'd like to be able to sign in
+} do
+
+  given(:user) { create(:user) }
+
+  scenario 'Registered user tries to sign out' do
+    sign_in(user)
+    visit root_path
+    click_on 'Log Out'
+    expect(page).to have_content 'Signed out successfully.'
+  end
+  scenario 'Unregistered user tries to sign out' do
+    visit root_path
+    expect(page).not_to have_selector(:link_or_button, 'Log Out')
+  end
+end
