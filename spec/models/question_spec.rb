de577e567febd3cc041ae67a051e8420b11842d3
Oleg Goloborodko <goloborodko@inbox.ru>
2020-02-09 19:24:51 +0300

    use active storage for one file attached

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of :body }
   it { should have_many(:answers) }
   it { should belong_to(:user) }
+
+  it 'have one attached file' do
+    expect(Question.new.file).to be_an_instance_of(ActiveStorage::Attached::One)
+  end
 end
