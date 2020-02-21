d83a6b2c3e4bc815da9bbe1a53c24415a141dcb8
Oleg Goloborodko <goloborodko@inbox.ru>
2020-02-21 13:53:27 +0300

    correcting migration

--- db/migrate/20200215065107_create_links.rb
+++ db/migrate/20200215065107_create_links.rb
@@ -1,8 +1,8 @@
 class CreateLinks < ActiveRecord::Migration[6.0]
   def change
     create_table :links do |t|
-      t.string :name
-      t.string :url
+      t.string :name, null: false
+      t.string :url, null: false
       t.belongs_to :linkable, polymorphic: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -49,8 +49,8 @@ ActiveRecord::Schema.define(version: 2020_02_15_065107) do
   end
 
   create_table "links", force: :cascade do |t|
-    t.string "name"
-    t.string "url"
+    t.string "name", null: false
+    t.string "url", null: false
     t.string "linkable_type"
     t.bigint "linkable_id"
     t.datetime "created_at", precision: 6, null: false
