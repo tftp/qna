ebc7f687347acff1e771016738f7bc325cc9a0b7
Oleg Goloborodko <goloborodko@inbox.ru>
2020-02-27 10:38:09 +0300

    correct views vote

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,8 +8,36 @@ module ApplicationHelper
     end
   end
 
-  def count_votes(resourse)
-    Vote.find_votables(votable: resourse).pluck(:value).sum
+  def output_count_votes(votable)
+    if votable.class.eql? Question
+      content_tag :div, "#{votable.count_votes}", class: "vote-mark-question", data: { question_id: votable.id }
+    else
+      content_tag :div, "#{votable.count_votes}", class: "vote-mark-answer", data: { answer_id: votable.id }
+    end
+  end
+
+  def vote_positive_link(votable)
+    if votable.class.eql? Question
+      link_to 'plus', polymorphic_path([:vote, votable], option: 'positive'),
+                class: "#{current_user&.is_author?(votable) ? 'hidden' : 'vote-pisitive-link'}",
+                data: { question_id: votable.id, type: :json }, remote: true, method: :patch
+    else
+      link_to 'plus', polymorphic_path([:vote, votable], option: 'positive'),
+                class: "#{current_user&.is_author?(votable) ? 'hidden' : 'vote-pisitive-link'}",
+                data: { answer_id: votable.id, type: :json }, remote: true, method: :patch
+    end
+  end
+
+  def vote_negative_link(votable)
+    if votable.class.eql? Question
+      link_to 'minus', polymorphic_path([:vote, votable], option: 'negative'),
+                class: "#{current_user&.is_author?(votable) ? 'hidden' : 'vote-negative-link'}",
+                data: { question_id: votable.id, type: :json }, remote: true, method: :patch
+    else
+      link_to 'minus', polymorphic_path([:vote, votable], option: 'negative'),
+                class: "#{current_user&.is_author?(votable) ? 'hidden' : 'vote-negative-link'}",
+                data: { answer_id: votable.id, type: :json }, remote: true, method: :patch
+    end
   end
 
 end
--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -5,4 +5,8 @@ module Votable
     has_many :votes, dependent: :destroy, as: :votable
   end
 
+  def count_votes
+    Vote.find_votables(votable: self).pluck(:value).sum
+  end
+
 end
