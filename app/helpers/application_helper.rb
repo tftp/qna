f0f68d84fc5139c132a1fa278efba839ad2ffbc7
Oleg Goloborodko <goloborodko@inbox.ru>
2020-02-27 12:31:33 +0300

    realized ajax json

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,19 +10,19 @@ module ApplicationHelper
 
   def output_count_votes(votable)
     if votable.class.eql? Question
-      content_tag :div, "#{votable.count_votes}", class: "vote-mark-question", data: { question_id: votable.id }
+      content_tag :div, "#{votable.count_votes}", class: "vote-mark-question", id: "#{votable.id}"
     else
-      content_tag :div, "#{votable.count_votes}", class: "vote-mark-answer", data: { answer_id: votable.id }
+      content_tag :div, "#{votable.count_votes}", class: "vote-mark-answer", id: "#{votable.id}"
     end
   end
 
   def vote_positive_link(votable)
     if votable.class.eql? Question
-      link_to 'plus', polymorphic_path([:vote, votable], option: 'positive'),
+      link_to 'plus', polymorphic_path([:voting, votable], option: 'positive'),
                 class: "#{current_user&.is_author?(votable) ? 'hidden' : 'vote-pisitive-link'}",
                 data: { question_id: votable.id, type: :json }, remote: true, method: :patch
     else
-      link_to 'plus', polymorphic_path([:vote, votable], option: 'positive'),
+      link_to 'plus', polymorphic_path([:voting, votable], option: 'positive'),
                 class: "#{current_user&.is_author?(votable) ? 'hidden' : 'vote-pisitive-link'}",
                 data: { answer_id: votable.id, type: :json }, remote: true, method: :patch
     end
@@ -30,11 +30,11 @@ module ApplicationHelper
 
   def vote_negative_link(votable)
     if votable.class.eql? Question
-      link_to 'minus', polymorphic_path([:vote, votable], option: 'negative'),
+      link_to 'minus', polymorphic_path([:voting, votable], option: 'negative'),
                 class: "#{current_user&.is_author?(votable) ? 'hidden' : 'vote-negative-link'}",
                 data: { question_id: votable.id, type: :json }, remote: true, method: :patch
     else
-      link_to 'minus', polymorphic_path([:vote, votable], option: 'negative'),
+      link_to 'minus', polymorphic_path([:voting, votable], option: 'negative'),
                 class: "#{current_user&.is_author?(votable) ? 'hidden' : 'vote-negative-link'}",
                 data: { answer_id: votable.id, type: :json }, remote: true, method: :patch
     end
--- app/javascript/channels/votes_channel.js
+++ app/javascript/channels/votes_channel.js
@@ -4,21 +4,36 @@ $(document).on('turbolinks:load', function(){
     var  vote = event.detail[0];
     var answerId = $(this).data('answerId');
     var questionId = $(this).data('questionId');
-    console.log(vote)
-    console.log(answerId)
-    console.log(questionId)
 
+    $('.vote-mark-question#'+questionId).html(vote)
+    $('.vote-mark-answer#'+answerId).html(vote)
+
+  })
+  .on('ajax:error', function(event){
+    var  errors = event.detail[0];
+
+    $.each(errors, function(index, value){
+      if (answerId) $('.answer-errors').append('<p>' + value + '</p>');
+      if (questionId) $('.question-errors').append('<p>' + value + '</p>');
+    })
   })
 
   $('a.vote-negative-link').on('ajax:success', function(event){
     var  vote = event.detail[0];
     var answerId = $(this).data('answerId');
     var questionId = $(this).data('questionId');
-    console.log(vote)
-    console.log(answerId)
-    console.log(questionId)
+
+    $('.vote-mark-question#'+questionId).html(vote)
+    $('.vote-mark-answer#'+answerId).html(vote)
 
   })
+  .on('ajax:error', function(event){
+    var  errors = event.detail[0];
 
+    $.each(errors, function(index, value){
+      if (answerId) $('.answer-errors').append('<p>' + value + '</p>');
+      if (questionId) $('.question-errors').append('<p>' + value + '</p>');
+    })
+  })
 
 });
