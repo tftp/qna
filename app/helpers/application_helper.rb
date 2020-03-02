c02dfe8a307a6ebe648a5e9e3737dd272a802928
Oleg Goloborodko <goloborodko@inbox.ru>
2020-03-02 18:50:17 +0300

    correct helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,20 +8,4 @@ module ApplicationHelper
     end
   end
 
-  def output_count_votes(votable)
-      content_tag :div, "#{votable.count_votes}", class: "vote-mark-#{votable.class.to_s.underscore}", id: "#{votable.id}"
-  end
-
-  def vote_positive_link(votable)
-    link_to 'plus', polymorphic_path([:vote, votable], option: 'positive'),
-              class: "#{current_user&.is_author?(votable) ? 'hidden' : 'vote-change-link'}",
-              data: { votable_type: votable.class.to_s.underscore ,votable_id: votable.id, type: :json }, remote: true, method: :patch
-  end
-
-  def vote_negative_link(votable)
-    link_to 'minus', polymorphic_path([:vote, votable], option: 'negative'),
-              class: "#{current_user&.is_author?(votable) ? 'hidden' : 'vote-change-link'}",
-              data: { votable_type: votable.class.to_s.underscore ,votable_id: votable.id, type: :json }, remote: true, method: :patch
-  end
-
 end
