2d7b182372c6f09214fca3127e9cb0afafab9412
Oleg Goloborodko <goloborodko@inbox.ru>
2020-03-16 20:36:33 +0300

    correct for OmniAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,28 @@ class User < ApplicationRecord
  has_many :answers, dependent: :destroy
  has_many :votes, dependent: :destroy
  has_many :comments, dependent: :destroy
+ has_many :authorizations
 
  validates :email, format: { with: /\A\w+@\w+\.[a-z]{2,3}\z/}
 
  def is_author?(obj)
    id == obj.user_id
+ end
+
+ def self.find_for_oauth(auth)
+   authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+   return authorization.user if authorization
+
+   email = auth.info[:email]
+   user = User.where(email: email).first
+   if user
+     user.authorizations.create(provider: auth.provider, uid: auth.uid)
+   else
+     password = Devise.friendly_token[0, 20]
+     user = User.create!(email: email, password: password, password_confirmation: password)
+     user.authorizations.create(provider: auth.provider, uid: auth.uid)
+   end
+   user
  end
 
 end
