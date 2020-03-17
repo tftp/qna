57dbbb2739863db87930e7abfb70b551d9e7b715
Oleg Goloborodko <goloborodko@inbox.ru>
2020-03-17 19:12:27 +0300

    add services refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
  has_many :answers, dependent: :destroy
  has_many :votes, dependent: :destroy
  has_many :comments, dependent: :destroy
- has_many :authorizations
+ has_many :authorizations, dependent: :destroy
 
  validates :email, format: { with: /\A\w+@\w+\.[a-z]{2,3}\z/}
 
@@ -18,19 +18,7 @@ class User < ApplicationRecord
  end
 
  def self.find_for_oauth(auth)
-   authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
-   return authorization.user if authorization
-
-   email = auth.info[:email]
-   user = User.where(email: email).first
-   if user
-     user.authorizations.create(provider: auth.provider, uid: auth.uid)
-   else
-     password = Devise.friendly_token[0, 20]
-     user = User.create!(email: email, password: password, password_confirmation: password)
-     user.authorizations.create(provider: auth.provider, uid: auth.uid)
-   end
-   user
+   Services::FindForOauth.new(auth).call
  end
 
 end
