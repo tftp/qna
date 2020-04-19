a5ec6fc346dcd1cdc15d992bb9bfb9bbf0a7bbe8
Oleg Goloborodko <goloborodko@inbox.ru>
2020-04-19 19:39:09 +0300

    correct specs for foundator and search_controller

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 RSpec.describe SearchController, type: :controller do
   describe 'GET#index' do
     let(:query) { 'string' }
+    let(:model) { 'question' }
 
     it 'initialize sphinx search' do
-      expect(ThinkingSphinx).to receive(:search).with(query)
-      get :index, params: {query: query, model: 'all'}
+      expect(Foundator).to receive(:search).with(query, model)
+      get :index, params: {query: query, model: model}
     end
 
     it 'renders index views' do
-      get :index, params: {query: query, model: 'question'}
+      allow(Foundator).to receive(:search).with(query, model).and_return([])
+      get :index, params: {query: query, model: model}
+
       expect(response).to render_template :index
     end
   end
