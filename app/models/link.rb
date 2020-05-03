7ec612e99f6baec82c4227e615e5ee5a80c8911f
OS <goloborodko@inbox.ru>
2020-05-03 13:50:19 +0300

    add cached question

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,5 @@
 class Link < ApplicationRecord
-  belongs_to :linkable, polymorphic: true
+  belongs_to :linkable, polymorphic: true, touch: true
 
   validates :name, :url, presence: true
   validates :url, format: { with: /\A(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})(\/[\w\.-]*)*\/?\z/}
